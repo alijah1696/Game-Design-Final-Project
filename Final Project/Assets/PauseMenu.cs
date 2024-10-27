2b4551e50f98a459ed76a1ab64a22137a31d2eec
Fcagua <fcagua01@tufts.edu>
2024-10-27 18:32:28 -0400

    Pause Button Functional

--- Final Project/Assets/PauseMenu.cs
+++ Final Project/Assets/PauseMenu.cs
@@ -35,7 +35,7 @@ public class PauseMenu : MonoBehaviour
     }
     public void BacktoMenu()
     {
-        SceneManager.LoadScene("Menu");
+        SceneManager.LoadScene("Main Menu");
     }
 
     public void QuitGame()
