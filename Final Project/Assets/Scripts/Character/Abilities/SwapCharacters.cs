2eea6670f7aad2aa19a7f0d818ee20e8f05d3668
alijah1696 <alijah1696@gmail.com>
2024-10-27 14:20:17 -0400

    Organize and Prototype
    added folders
    
    started working on prototype scene

---
+++ Final Project/Assets/Scripts/Character/Abilities/SwapCharacters.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapCharacters : MonoBehaviour
+{
+    public GameObject plant;      // The first character (mutated plant)
+    public GameObject robot;      // The second character (robot)
+
+    private GameObject activeCharacter; // Currently active character
+
+    void Start()
+    {
+        // Start with the plant as the active character by default
+        activeCharacter = plant;
+        SetActiveCharacter(plant);
+
+        // Ensure the other character is inactive
+        robot.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Swap characters when Tab is pressed
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            SwapCharacter();
+        }
+    }
+
+    void SwapCharacter()
+    {
+        // Toggle the active character
+        if (activeCharacter == plant)
+        {
+            SetActiveCharacter(robot);
+        }
+        else
+        {
+            SetActiveCharacter(plant);
+        }
+    }
+
+    void SetActiveCharacter(GameObject characterToActivate)
+    {
+        // Transfer position
+        characterToActivate.transform.position = activeCharacter.transform.position;
+
+        // Transfer velocity
+        Rigidbody2D currentRb = activeCharacter.GetComponent<Rigidbody2D>();
+        Rigidbody2D newRb = characterToActivate.GetComponent<Rigidbody2D>();
+        newRb.velocity = currentRb.velocity;
+
+        // Deactivate the previous character and activate the new one
+        activeCharacter.SetActive(false);
+        characterToActivate.SetActive(true);
+
+        // Update the active character reference
+        activeCharacter = characterToActivate;
+    }
+}
---
+++ Final Project/Assets/Scripts/Character/Movement/MoveCharacter.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Get input for horizontal movement (A/D or Arrow Keys)
+        movement.x = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        // Preserve vertical velocity (e.g., for gravity) while setting horizontal velocity
+        rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
+    }
+
+    void OnDisable()
+    {
+        // Ensure the character stops moving horizontally when swapped out
+        rb.velocity = new Vector2(0, rb.velocity.y);
+    }
+}
