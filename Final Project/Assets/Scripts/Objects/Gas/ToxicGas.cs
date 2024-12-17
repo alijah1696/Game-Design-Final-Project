e82eb3f9b5ce11b73c2795b778abfd81f9846f27
Fcagua <fcagua01@tufts.edu>
2024-12-16 19:45:54 -0500

    water and gas sounds effects

--- Final Project/Assets/Scripts/Objects/Gas/ToxicGas.cs
+++ Final Project/Assets/Scripts/Objects/Gas/ToxicGas.cs
@@ -4,38 +4,75 @@ using UnityEngine;
 
 public class ToxicGas : MonoBehaviour
 {
-    public GameObject respawnPoint;
-    SwapCharacters sc;
+    public GameObject respawnPoint;  // The respawn point if the player dies
+    private SwapCharacters sc;       // Reference to the character swapping script
+    private AudioManager audioManager;  // Reference to AudioManager for playing sounds
+    private bool isPlantInGas = false;  // To track whether the plant is still in the gas
+    private bool isRobotInGas = false;  // To track whether the robot is still in the gas
 
     void Start()
     {
         sc = FindObjectOfType<SwapCharacters>();
-    }
-
-    void Update()
-    {
-        
+        audioManager = FindObjectOfType<AudioManager>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
-    {   
+    {
+        if (other.CompareTag("Plant") || other.CompareTag("Robot"))
+        {
+            StartGasEffects();
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
         if (other.CompareTag("Plant"))
-        {   
-            sc.Kill(respawnPoint);
+        {
+            isPlantInGas = true;  // Confirm the plant is still in the gas
         }
         else if (other.CompareTag("Robot"))
         {
-            MoveCharacter mv = sc.GetCurrentForm().GetComponent<MoveCharacter>();
-            mv.InDanger();
+            isRobotInGas = true;  // Confirm the robot is still in the gas
         }
     }
 
     void OnTriggerExit2D(Collider2D other)
-    {   
-        if (other.CompareTag("Robot"))
+    {
+        if (other.CompareTag("Plant"))
         {
-            MoveCharacter mv = sc.GetCurrentForm().GetComponent<MoveCharacter>();
-            mv.Safe();
+            StopGasEffects();
+            isPlantInGas = false;
         }
+        else if (other.CompareTag("Robot"))
+        {
+            StopGasEffects();
+            isRobotInGas = false;
+        }
+    }
+
+    void Update()
+    {
+        // Check to ensure that the gas sound continues if the characters are stationary
+        if ((isPlantInGas || isRobotInGas) && !audioManager.IsPlayingToxicGasSound())
+        {
+            StartGasEffects();
+        }
+
+        // Reset flags unless OnTriggerStay2D sets them again
+        isPlantInGas = false;
+        isRobotInGas = false;
+    }
+
+    private void StartGasEffects()
+    {
+        if (!audioManager.IsPlayingToxicGasSound())
+        {
+            audioManager.PlayToxicGasSound();
+        }
+    }
+
+    private void StopGasEffects()
+    {
+        audioManager.StopToxicGasSound();
     }
 }
