809c266fb35b31465ae4fdae6f31e25ad7f0906f
DelaneyHC <delaneyskye@icloud.com>
2024-12-16 23:10:06 -0500

    Revert "bgbg"
    This reverts commit 07739774cc2fdd1a30b008b41070d5089151f105.

--- Final Project/Assets/Scripts/Objects/Gas/ToxicGas.cs
+++ Final Project/Assets/Scripts/Objects/Gas/ToxicGas.cs
@@ -4,75 +4,38 @@ using UnityEngine;
 
 public class ToxicGas : MonoBehaviour
 {
-    public GameObject respawnPoint;  // The respawn point if the player dies
-    private SwapCharacters sc;       // Reference to the character swapping script
-    private AudioManager audioManager;  // Reference to AudioManager for playing sounds
-    private bool isPlantInGas = false;  // To track whether the plant is still in the gas
-    private bool isRobotInGas = false;  // To track whether the robot is still in the gas
+    public GameObject respawnPoint;
+    SwapCharacters sc;
 
     void Start()
     {
         sc = FindObjectOfType<SwapCharacters>();
-        audioManager = FindObjectOfType<AudioManager>();
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Plant") || other.CompareTag("Robot"))
-        {
-            StartGasEffects();
-        }
-    }
-
-    void OnTriggerStay2D(Collider2D other)
-    {
-        if (other.CompareTag("Plant"))
-        {
-            isPlantInGas = true;  // Confirm the plant is still in the gas
-        }
-        else if (other.CompareTag("Robot"))
-        {
-            isRobotInGas = true;  // Confirm the robot is still in the gas
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Plant"))
-        {
-            StopGasEffects();
-            isPlantInGas = false;
-        }
-        else if (other.CompareTag("Robot"))
-        {
-            StopGasEffects();
-            isRobotInGas = false;
-        }
     }
 
     void Update()
     {
-        // Check to ensure that the gas sound continues if the characters are stationary
-        if ((isPlantInGas || isRobotInGas) && !audioManager.IsPlayingToxicGasSound())
-        {
-            StartGasEffects();
-        }
-
-        // Reset flags unless OnTriggerStay2D sets them again
-        isPlantInGas = false;
-        isRobotInGas = false;
+        
     }
 
-    private void StartGasEffects()
-    {
-        if (!audioManager.IsPlayingToxicGasSound())
+    void OnTriggerEnter2D(Collider2D other)
+    {   
+        if (other.CompareTag("Plant"))
+        {   
+            sc.Kill(respawnPoint);
+        }
+        else if (other.CompareTag("Robot"))
         {
-            audioManager.PlayToxicGasSound();
+            MoveCharacter mv = sc.GetCurrentForm().GetComponent<MoveCharacter>();
+            mv.InDanger();
         }
     }
 
-    private void StopGasEffects()
-    {
-        audioManager.StopToxicGasSound();
+    void OnTriggerExit2D(Collider2D other)
+    {   
+        if (other.CompareTag("Robot"))
+        {
+            MoveCharacter mv = sc.GetCurrentForm().GetComponent<MoveCharacter>();
+            mv.Safe();
+        }
     }
 }
